e3e4b3b42581ca0c81b9b121e4aba3dda8a17e19
jacmoe <jacmoe.dk@gmail.com>
2023-10-04 08:50:47 +0200

    IPA script translates to Duployan now

--- ipa.sh
+++ ipa.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 	curl -sS https://dictionaryapi.com/api/v3/references/collegiate/json/$1\?key=$WEBSTER_KEY\
-	| jq . | awk '/mw/ { gsub("\"", ""); gsub(",", ""); sub(/^./, "", $2); print $2; exit}'\
+	| jq . | awk '/mw/ { gsub("\"", ""); gsub("au̇", "[15]"); gsub("ȯi", "[16]"); gsub("yü", "[17]"); gsub("a", "[1]"); \
+	gsub("ä", "[3]"); gsub("ȯ", "[4]"); gsub("ō", "[5]"); gsub("u̇", "[6]"); gsub("ü", "[7]"); gsub("ər", "[9]"); gsub("ə", "[8]"); \
+	gsub("e", "[10]"); gsub("ā", "[11]"); gsub("i", "[12]"); gsub("ē", "[13]"); gsub("ī", "[14]"); \
+	gsub(",", ""); sub(/^./, "", $2); print $2; exit}'\
 	| xargs -I {} echo $1 {}
