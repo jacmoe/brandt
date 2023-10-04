96ca9c3f9ab750c85da4b6484417420e274dc6d5
jacmoe <jacmoe.dk@gmail.com>
2023-10-04 09:31:08 +0200

    Better IPA script

--- ipa.sh
+++ ipa.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 	curl -sS https://dictionaryapi.com/api/v3/references/collegiate/json/$1\?key=$WEBSTER_KEY\
-	| jq . | awk '/mw/ { gsub("\"", ""); gsub("au̇", "[15]"); gsub("ȯi", "[16]"); gsub("yü", "[17]"); gsub("a", "[1]"); \
-	gsub("ä", "[3]"); gsub("ȯ", "[4]"); gsub("ō", "[5]"); gsub("u̇", "[6]"); gsub("ü", "[7]"); gsub("ər", "[9]"); gsub("ə", "[8]"); \
-	gsub("e", "[10]"); gsub("ā", "[11]"); gsub("i", "[12]"); gsub("ē", "[13]"); gsub("ī", "[14]"); \
+	| jq . | awk '/mw/ { gsub("\"", ""); gsub("au̇", "[OW]"); gsub("ȯi", "[OI]"); gsub("yü", "[JU]"); gsub("a", "[AH_s]"); \
+	gsub("ä", "[AW_s]"); gsub("ȯ", "[AW_l]"); gsub("ō", "[O]"); gsub("u̇", "[OO_s]"); gsub("ü", "[OO_l]"); gsub("ər", "[UR]"); gsub("ə", "[UH]"); \
+	gsub("e", "[EH]"); gsub("ā", "[EY]"); gsub("i", "[EE_s]"); gsub("ē", "[EE_l]"); gsub("ī", "[AI]"); \
 	gsub(",", ""); sub(/^./, "", $2); print $2; exit}'\
 	| xargs -I {} echo $1 {}
