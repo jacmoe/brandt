3417e8c70847845da4c7c7b56984186fa0652735
jacmoe <jacmoe.dk@gmail.com>
2023-10-13 18:14:27 +0200

    build

---
+++ docs/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Brandt's Duployan",
+    "uri": "/"
+  },
+  {
+    "breadcrumb": "Brandt's Duployan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/"
+  },
+  {
+    "breadcrumb": "Brandt's Duployan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/"
+  }
+]
